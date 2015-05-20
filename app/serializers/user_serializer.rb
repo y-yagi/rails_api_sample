083dc20959e617ba117c0e65a8b5e09ee4ad621f
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-05-20 19:19:07 +0900

    add attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id
+  attributes :name
+  attributes :email
+  attributes :url
+
+  def url
+    "http://localhost:3001/users/#{id}.json"
+  end
 end
